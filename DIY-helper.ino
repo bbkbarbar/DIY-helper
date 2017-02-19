ff0a3aeb8b9643d53fa4604d1c40aff6decc33a6
bbkbarbar <bbk.barbar@gmail.com>
2017-02-19 14:00:43 +0100

    Define macros for modes

--- DIY-helper.ino
+++ DIY-helper.ino
@@ -1,26 +1,52 @@
 #include <buttonhandler.h>
 
-// pin definitions
-#define OLED_D0           8
-#define OLED_D1           7
-#define OLED_RST          6
-#define OLED_DC           5
-#define OLED_CS           4
+// Pin definitions
+//   Display
+#define OLED_D0              8
+#define OLED_D1              7
+#define OLED_RST             6
+#define OLED_DC              5
+#define OLED_CS              4
+  
+//   Buttons  
+#define ADC_FOR_BUTTONS     A5
 
-#define ADC_FOR_BUTTONS  A5
+//   Disctance sensors
+
+
+// Buttons
+#define NUMBER_OF_BUTTONS    3
+#define BTN1                 1
+#define BTN2                 2
+#define BTN3                 3
+
+// Modes
+#define MODE_COUNT           4
+#define MODE_SDM             0
+#define MODE_OSM             1
+#define MODE_POS             2
+#define MODE_AREA            3
+
+// Directions
+#define DIRECTION_UP         0
+#define DIRECTION_RIGHT      1
+#define DIRECTION_DOWN       2
+#define DIRECTION_LEFT       3
+
+#define DIRECTION_VERTICAL   4
+#define DIRECTION_HORIZONTAL 5
+
 
 #include <SSD1306Ascii.h>
 #include <SSD1306AsciiSoftSpi.h>
 
-#define NUMBER_OF_BUTTONS 3
-#define BTN1              1
-#define BTN2              2
-#define BTN3              3
-
-
+// Global variables
 
 ButtonHandler buttons;
 SSD1306AsciiSoftSpi oled;
+
+
+// Functions
 
 void setup() {
 
@@ -33,37 +59,9 @@ void setup() {
   
 }
 
-int ispressed = 0;
 
 void loop() {
 
-  if(buttons.isPressed(BTN1)){
-    if(ispressed != 1){
-      digitalWrite(13, HIGH);
-      oled.clear();
-      oled.println("Btn1 pressed\n");
-    }
-    ispressed = 1;
-  }
-  else
-  if(buttons.isPressed(BTN2)){
-    if(ispressed != 2){
-      digitalWrite(13, LOW);
-      oled.clear();
-      oled.println("Btn2 pressed\n");
-    }
-    ispressed = 2;
-  }
-  else
-  if(buttons.isPressed(BTN3)){
-    if(ispressed != 3){
-      oled.clear();
-      oled.println("Btn3 pressed\n");
-    }
-    ispressed = 3;
-  }else{
-    ispressed = 0;
-    oled.clear();
-  }
+
 
 }
